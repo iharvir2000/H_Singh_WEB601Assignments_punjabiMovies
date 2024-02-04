f5d156240817db5e98694dd72b10e4fb5f2743ea
iharvir2000 <137436443+iharvir2000@users.noreply.github.com>
2024-02-03 23:53:32 -0500

    final commit

--- H_Singh_punjabiMovies/src/app/content-card/content-card.component.ts
+++ H_Singh_punjabiMovies/src/app/content-card/content-card.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
+
 
 
 @Component({
@@ -10,32 +10,4 @@ import { ContentList } from '../helper-files/content-list';
   styleUrl: './content-card.component.scss'
 })
 export class ContentCardComponent {
-  contentList: ContentList = new ContentList();
-
-  constructor(){
-    this.contentList.addContent({
-      id: 0,
-      title: "Angrej",
-      description:"A thoughtful young man struggles to find a woman to marry.",
-      creator:"	Simerjit Singh",
-      imgURL:"https://upload.wikimedia.org/wikipedia/en/2/2a/Angrej_poster.jpg",
-      type:"Romantic"
-    });
-    this.contentList.addContent({
-      id: 1,
-      title: "Sardar Mohammad",
-      description:"Set during the India-Pakistan partition, the story of Sardar Mohammad revolves around an infant who is saved by an Indian Sikh police officer.",
-      creator:"Harry Bhatti",
-      imgURL:"https://www.lavanguardia.com/peliculas-series/images/movie/poster/2017/11/w1280/znif778ztwOmKssPrXL0m42gU2Y.jpg",
-      type:"Biography"
-    });
-    this.contentList.addContent({
-      id: 2,
-      title: "Shareek",
-      description:"The word 'Shareek' may refer to 'sharing' in dictionary but in Punjab it had always been opposite to it. As time flies, one doesn't even notice that how and when the brothers become the enemies of each other.",
-      creator:"Navaniat Singh",
-      imgURL:"https://upload.wikimedia.org/wikipedia/en/2/2f/Shareek_movie_poster.jpg",
-      type:"Drama"
-    });
-  }
 }
---
+++ H_Singh_punjabiMovies/src/app/content-list/content-list.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+
+@Component({
+  selector: 'app-content-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './content-list.component.html',
+  styleUrl: './content-list.component.scss'
+})
+export class ContentListComponent implements OnInit {
+  displayContentInfo(contentItem: Content) {
+    console.log(`ID: ${contentItem.id} and Title: ${contentItem.title}`);
+    }
+  @Input () contentItems: Content[] = [];
+
+  ngOnInit(): void {
+    this.contentItems =[
+      {
+        id: 0,
+        title: "Angrej",
+        description:"A thoughtful young man struggles to find a woman to marry.",
+        creator:"	Simerjit Singh",
+        imgURL:"https://upload.wikimedia.org/wikipedia/en/2/2a/Angrej_poster.jpg",
+        type:"Romantic",
+        tags: ["romantic", "comady"]
+      },
+      {
+        id: 1,
+        title: "Sardar Mohammad",
+        description:"Set during the India-Pakistan partition, the story of Sardar Mohammad revolves around an infant who is saved by an Indian Sikh police officer.",
+        creator:"Harry Bhatti",
+        imgURL:"https://timesofindia.indiatimes.com/photo/61486918.cms",
+        type:"Biography",
+        tags: ["Biography", "Drama"]
+      },
+      {
+        id: 2,
+        title: "Shareek",
+        description:"The word 'Shareek' may refer to 'sharing' in dictionary but in Punjab it had always been opposite to it. As time flies, one doesn't even notice that how and when the brothers become the enemies of each other.",
+        creator:"Navaniat Singh",
+        imgURL:"https://upload.wikimedia.org/wikipedia/en/2/2f/Shareek_movie_poster.jpg",
+        type:"Drama",
+        tags: ["Drama", "Comady"]
+      }
+      
+    ];
+  }
+ 
+}
